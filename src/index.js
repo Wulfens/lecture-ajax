1d4943a0051140d4f40529182914332552954ea5
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2023-04-15 11:34:22 +0200

    lecture 1170

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log("Hello from src/index.js!");
+// console.log("Hello from src/index.js!");
+// const button = document.querySelector("#clickme");
+// // const button = document.getElementById("clickme");
+
+// console.log(button);
+
+// // button.addEventListener("EVENT_TYPE", "CALLBACK");
+// // button.addEventListener("click", (event) => {});
+
+// button.addEventListener("click", (event) => {
+//   event.currentTarget.innerText = "Hold Still...";
+//   event.currentTarget.setAttribute("disabled", "");
+//   // event.currentTarget.disable = true;
+// });
+
+const searchMovies = (query) => {
+  const moviesList = document.getElementById("movies-list");
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.Search);
+      moviesList.innerHTML = "";
+      data.Search.forEach((movie) => {
+        const movieHtml = `<li class="list-inline-item">
+                            <img src="${movie.Poster}" alt="">
+                            <p>${movie.Title}</p>
+                          </li>`;
+        moviesList.insertAdjacentHTML('beforeend', movieHtml);
+      });
+    });
+};
+
+const form = document.querySelector("#search-form");
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const inputQuery = form.querySelector("#search-input").value;
+  searchMovies(inputQuery);
+});
